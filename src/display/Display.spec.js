b06a1967d861560aea5a9e4384020b1b89b7ea35
Christopher Ksiazak <cksiazak@gmail.com>
2019-10-02 19:36:16 -0600

    updated modules, added @testing-library, and finished Display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+test('Display component renders completely', () => {
+  render(<Display />);
+});
+
+test("Display renders 'closed' upon closed prop being true", () => {
+  const closed = true;
+
+  const { getByText } = render(<Display closed={closed} />);
+  getByText(/closed/i);
+});
+
+test("Display renders 'locked' upon locked prop being true", () => {
+  const locked = true;
+
+  const { getByText } = render(<Display locked={locked} />);
+  getByText(/locked/i);
+});
