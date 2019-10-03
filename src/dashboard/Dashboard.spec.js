e55ab373013331ebb69156410467423ec01403fe
Christopher Ksiazak <cksiazak@gmail.com>
2019-10-02 19:45:24 -0600

    added dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test('Dashboard renders', () => {
+  render(<Dashboard />);
+});
+
+test('Dashboard renders with gate', () => {
+  const { getByText } = render(<Dashboard />);
+  getByText(/unlocked/i);
+  getByText(/lock gate/i);
+  getByText(/open/i);
+  getByText(/close gate/i);
+});
