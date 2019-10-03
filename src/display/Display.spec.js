9e1201194d90c724bc3425a24830f1b330d701dd
Christopher Ksiazak <cksiazak@gmail.com>
2019-10-02 20:13:47 -0600

    added className renderers

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 // Test away!
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import Display from './Display';
 
@@ -22,4 +23,16 @@ test("Display renders 'locked' upon locked prop being true", () => {
   //This component has locked = true, thus should render 'locked'
   const { getByText } = render(<Display locked={locked} />);
   getByText(/locked/i);
+});
+
+it('Classname of "red-led" is chosed when closed/locked is true', () => {
+  const component = render(<Display closed={true} locked={false} />);
+  const disp = component.getByText(/closed/i);
+  expect(disp).toHaveClass('red-led');
+});
+
+it('Classname of "green-led" is chosed when open is true', () => {
+  const component = render(<Display closed={false} locked={false} />);
+  const disp = component.getByText(/open/i);
+  expect(disp).toHaveClass('green-led');
 });
